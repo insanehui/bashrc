6ae1cec51b90bf2aa28547c7996e150088ded818
insanehui <269200808@qq.com>
2017-08-01 15:41:54 +0800

    修改一下git clone的目录

--- init.sh
+++ init.sh
@@ -14,6 +14,6 @@ cat << EOF > ~/.bashrc
 EOF
 
 # 初始化vim
-git clone https://github.com/insanehui/vimfiles.git
+git clone https://github.com/insanehui/vimfiles.git ~/vimfiles
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
